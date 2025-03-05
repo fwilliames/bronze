03a4833913e1fdca4b8fc9a0df56bfa8179a249b
Frank Williames <dev.frankw@gmail.com>
2025-03-05 14:32:44 -0400

    feat: adicionado getproductsbyfilter

--- internal/application/services/user_service.go
+++ internal/application/services/user_service.go
@@ -33,6 +33,14 @@ func (s *UserService) GetProducts() ([]domain.Product, error) {
 	return users, nil
 }
 
+func (s *UserService) GetProductsByFilter(filter string) ([]domain.Product, error) {
+	users, err := s.repo.GetAllProductsbyFilter(filter) // Este método deve ser implementado no seu repositório
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
+
 func (s *UserService) GenerateReport() error {
 	println("Generate Report")
 	products, err := s.GetProducts()
