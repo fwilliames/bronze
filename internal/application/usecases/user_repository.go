6555d8e7a7700bf715d552357b355007b1e98745
Frank Williames <dev.frankw@gmail.com>
2025-03-05 14:33:04 -0400

    feat: adicionado getproductsbyfilter

--- internal/application/usecases/user_repository.go
+++ internal/application/usecases/user_repository.go
@@ -5,5 +5,6 @@ import "bronze/internal/domain"
 type UserRepository interface {
 	SaveProduct(name, data string, age float64) error
 	GetAllProducts() ([]domain.Product, error)
+	GetAllProductsbyFilter(filter string) ([]domain.Product, error)
 	GetUniqueDates() ([]string, error)
 }
