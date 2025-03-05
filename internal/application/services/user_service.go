65b41662b3f57f5f1f30a609dbf2f2996c23d0ff
Frank Williames <dev.frankw@gmail.com>
2025-03-05 12:28:18 -0400

    feat:adicionado campo data

--- internal/application/services/user_service.go
+++ internal/application/services/user_service.go
@@ -20,8 +20,8 @@ func NewUserService(repo usecases.UserRepository) *UserService {
 }
 
 // SaveUser salva um usuário no banco
-func (s *UserService) SaveProduct(name string, value float64) error {
-	return s.repo.SaveProduct(name, value)
+func (s *UserService) SaveProduct(name, data string, value float64) error {
+	return s.repo.SaveProduct(name, data, value)
 }
 
 // GetUsers retorna a lista de usuários cadastrados
@@ -50,14 +50,26 @@ func (s *UserService) GenerateReport() error {
 
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
+
+	// Título principal
 	pdf.SetFont("Arial", "B", 16)
 	pdf.Cell(200, 10, "Lista de Produtos")
-	pdf.Ln(20)
+	pdf.Ln(20) // Pula uma linha
+
+	// Títulos das colunas
+	pdf.SetFont("Arial", "B", 12)
+	pdf.Cell(66, 10, "Produto")
+	pdf.Cell(66, 10, "Valor")
+	pdf.Cell(66, 10, "Mes/Ano")
+	pdf.Ln(10) // Pula uma linha
+
+	// Definir a fonte para os dados
 	pdf.SetFont("Arial", "", 12)
 
 	for _, product := range products {
-		pdf.Cell(100, 10, fmt.Sprintf("Produto: %s", product.Name))
-		pdf.Cell(0, 10, fmt.Sprintf("Valor: %.2f", product.Value))
+		pdf.Cell(66, 10, product.Name)
+		pdf.Cell(66, 10, fmt.Sprintf("%.2f", product.Value))
+		pdf.Cell(66, 10, product.Data)
 		pdf.Ln(10)
 	}
 
