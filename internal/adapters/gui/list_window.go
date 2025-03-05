3d76d5b37453f2c6ade1e1510721be38eb532ba9
Frank Williames <dev.frankw@gmail.com>
2025-03-05 13:34:58 -0400

    feat: adicionado elemento select a tela de listar produtos

--- internal/adapters/gui/list_window.go
+++ internal/adapters/gui/list_window.go
@@ -12,10 +12,12 @@ func NewListWindow(g *services.GUIService, w fyne.Window) fyne.CanvasObject {
 	productsList := container.NewVBox()
 	insertButton := CreateInsertButton(g, w)
 	reportButton := CreateReportButton(g, w)
+	dataSelectFilter := CreateDataSelectFilter(g, w)
 
 	g.RefreshUserList(productsList)
 
 	return container.NewVBox(
+		dataSelectFilter,
 		productsList,
 		insertButton,
 		reportButton,
