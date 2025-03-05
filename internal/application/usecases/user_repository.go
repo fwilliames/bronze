6b704050e8b4b6f0a74c21148243e1cbdbeb2f8f
Frank Williames <dev.frankw@gmail.com>
2025-03-05 12:28:55 -0400

    feat: adicionado campo data

--- internal/application/usecases/user_repository.go
+++ internal/application/usecases/user_repository.go
@@ -3,6 +3,6 @@ package usecases
 import "bronze/internal/domain"
 
 type UserRepository interface {
-	SaveProduct(name string, age float64) error
+	SaveProduct(name, data string, age float64) error
 	GetAllProducts() ([]domain.Product, error)
 }
