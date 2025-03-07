bdf469d769651278b58aad384e76dee718964609
Frank Williames <dev.frankw@gmail.com>
2025-03-06 20:38:06 -0400

    chore: removendo comentarios

--- internal/application/services/user_service.go
+++ internal/application/services/user_service.go
@@ -9,38 +9,32 @@ import (
 	"github.com/jung-kurt/gofpdf"
 )
 
-// UserService implementa os casos de uso
 type UserService struct {
 	repo usecases.UserRepository
 }
 
-// NewUserService cria uma nova instância do serviço
 func NewUserService(repo usecases.UserRepository) *UserService {
 	return &UserService{repo: repo}
 }
 
-// SaveUser salva um usuário no banco
 func (s *UserService) SaveProduct(name, data, market string, value float64) error {
 	return s.repo.SaveProduct(name, data, market, value)
 }
 
-// SaveUser salva um usuário no banco
 func (s *UserService) SaveMarket(name string) error {
 	return s.repo.SaveMarket(name)
 }
 
-// GetUsers retorna a lista de usuários cadastrados
 func (s *UserService) GetProducts() ([]domain.Product, error) {
-	users, err := s.repo.GetAllProducts() // Este método deve ser implementado no seu repositório
+	users, err := s.repo.GetAllProducts()
 	if err != nil {
 		return nil, err
 	}
 	return users, nil
 }
 
-// GetUsers retorna a lista de usuários cadastrados
 func (s *UserService) GetMarkets() ([]string, error) {
-	markets, err := s.repo.GetAllMarkets() // Este método deve ser implementado no seu repositório
+	markets, err := s.repo.GetAllMarkets()
 	if err != nil {
 		return nil, err
 	}
@@ -48,7 +42,7 @@ func (s *UserService) GetMarkets() ([]string, error) {
 }
 
 func (s *UserService) GetProductsByFilter(filter string) ([]domain.Product, error) {
-	users, err := s.repo.GetAllProductsbyFilter(filter) // Este método deve ser implementado no seu repositório
+	users, err := s.repo.GetAllProductsbyFilter(filter)
 	if err != nil {
 		return nil, err
 	}
